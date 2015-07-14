9a4f4c5d524363066748542d7d78acbd9fbcaded
Derek Chung <derektchung12@yahoo.com>
2015-07-14 13:32:39 -0700

    Frontend and Backend can communicate through multiple buttons

--- gocode/src/github.com/nf/reddit/main.go
+++ gocode/src/github.com/nf/reddit/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+//	"io/ioutil"
 	"net/http"
+//	"log"
 )
-
+/*
 type Page struct {
 	Title string
 	Body  []byte
@@ -23,14 +24,39 @@ func loadPage(title string) (*Page, error) {
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
-}
+}*/
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	//fmt.Println(title)
+	command := r.URL.Path[len("/view/"):]
+	
+	/*
 	p, _ := loadPage(title)
+	resp, err := http.Head("http://google.com/")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+
+	if err != nil {
+    	log.Fatal(err)
+	}
+
+	fmt.Println(resp)
+	fmt.Println(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)//how to respond to the frontend
+	*/
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	//fmt.Fprintf(w,command)
+
+	switch command {
+	    case "setIp1":
+	    	fmt.Fprintf(w, "setIp1")
+	    case "setIp2":
+	    	fmt.Fprintf(w, "setIp2")
+	    case "sumIp":
+	    	fmt.Fprintf(w, "sumIp")
+	    default:
+	        fmt.Fprintf(w, "error has occurred")
+    }
 }
 
 func main() {
--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,31 @@ $(document).ready(function () {
         $(this).parent().children('ul.tree').toggle(300);
     });
 
-    $('button').click(function () {
-        $.get("http://localhost:8080/view/TestPage", function(data) {
+    $('#firstbutton').click(function () {
+        $.get("http://localhost:8080/view/sumIp", function(data) {
   			alert(data);
-	});    });
+		});
+	});
+
+	$('#ip1button').click(function () {
+        $.get("http://localhost:8080/view/setIp1", function(data) {
+  			alert(data);
+		});
+    });
+
+
+	/*function myFunction() {
+    var x = document.getElementById("frm1");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+        text += x.elements[i].value + "<br>";
+    }
+    document.getElementById("demo").innerHTML = text;
+}	*/
+
+
+
 
 
     $('.collapse').collapse()
